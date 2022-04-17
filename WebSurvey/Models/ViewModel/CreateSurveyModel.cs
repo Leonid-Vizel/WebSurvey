3f6d63af1f4fee3603c6bc7ccf5a14b23e583d5d
Leonid-Vizel <spartanlaska@gmail.com>
2022-04-17 13:36:23 +0300

    Исправлены ошибки валидации при создании голосований

--- WebSurvey/Models/ViewModel/CreateSurveyModel.cs
+++ WebSurvey/Models/ViewModel/CreateSurveyModel.cs
@@ -1,9 +1,13 @@
-﻿namespace WebSurvey.Models.ViewModel
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebSurvey.Models.ViewModel
 {
     public class CreateSurveyModel : Database.Survey
     {
         public CreateSurveyModel() { /*Пустой конструктор*/ }
 
+        [Required(ErrorMessage = "У опроса должен быть как минимум 1 вопрос")]
+        [MinLength(2, ErrorMessage = "У опроса должен быть как минимум 1 вопрос")]
         public SurveyQuestion[] Questions { get; set; }
     }
 }
--- WebSurvey/Models/Voting/VotingCreateModel.cs
+++ WebSurvey/Models/Voting/VotingCreateModel.cs
@@ -4,8 +4,8 @@ namespace WebSurvey.Models.Voting
 {
     public class VotingCreateModel : Voting
     {
-        [Required(ErrorMessage = "У опроса должены быть как минимум 2 опции")]
-        [MinLength(2,ErrorMessage = "У опроса должены быть как минимум 2 опции")]
+        [Required(ErrorMessage = "У голосования должены быть как минимум 2 опции")]
+        [MinLength(2,ErrorMessage = "У голосования должены быть как минимум 2 опции")]
         public List<VotingOption> Options { get; set; }
     }
 }
